0795fd4618e2eb08cda9c5911f23b4b1ffcdd141
AhmedReda178 <rda49586@gmail.com>
2026-08-29 02:28:36 +0300

    StoreDbConnection.Program

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -1,4 +1,7 @@
 
+using ECommerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ECommerce.API
 {
     public class Program
@@ -8,7 +11,10 @@ namespace ECommerce.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddDbContext<StoreDbcontext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("StoreDbConnection"));
+            });
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
