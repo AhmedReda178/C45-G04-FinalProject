486038ccb38690243ea36b4cd1fcf6cf7a69956f
AhmedReda178 <rda49586@gmail.com>
2026-08-29 01:24:09 +0300

    Product Module

---
+++ ECommerce.Domain/Entities/Products/ProductsBrand.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.Entities.Products
+{
+    public class ProductsBrand : BaseEntity<int>
+    {
+        public string Name { get; set; } = null!;
+    }
+}
