607b0cf3b74ace017eaa2f29395547217b1e1214
AhmedReda178 <rda49586@gmail.com>
2026-08-29 02:32:31 +0300

    InfraStructure(StoreDbcontext)

---
+++ ECommerce.Infrastructure/Data/StoreDbcontext.cs
@@ -0,0 +1,25 @@
+﻿using ECommerce.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Infrastructure.Data
+{
+    public class StoreDbcontext:DbContext
+    {
+        public StoreDbcontext(DbContextOptions<StoreDbcontext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductsBrand> ProductsBrands { get; set; }
+        public DbSet<ProductsType> ProductsTypes   { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreDbcontext).Assembly);
+        }
+
+    }
+}
