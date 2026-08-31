7550adfb56f81f80226f3fc6811fe9431696d562
AhmedReda178 <rda49586@gmail.com>
2026-08-31 03:36:45 +0300

    ApplicationServicesRegistrations

---
+++ ECommerce.Application/ApplicationServicesRegistrations.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Application.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Application
+{
+    public static class ApplicationServicesRegistrations
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(c => c.AddProfile(new ProductProfile()));
+            return services;
+        }
+    }
+}
